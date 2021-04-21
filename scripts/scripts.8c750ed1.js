0d59c09ece8b218d5e00fef2e7ed445f5b370cc3
willbokulet <37994506+willbokulet@users.noreply.github.com>
2021-04-21 03:45:02 -0500

    Update scripts.8c750ed1.js

--- scripts/scripts.8c750ed1.js
+++ scripts/scripts.8c750ed1.js
@@ -134,204 +134,547 @@ app.config(["$stateProvider", "$urlRouterProvider", "localStorageServiceProvider
     return [{
       id: 1511,
       map: "~will",
-      name: "Willbo"
+      name: "Willbo",
+      respawn: {
+        min: 2700,
+        max: 4500
+      }
     },{
       id: 2362,
-      map: "Bongo Earthquake",
-      name: "Bongo"
+      map: "moc_prydn2",
+      name: "Amon Ra (Nightmare)",
+      respawn: {
+        min: 2700,
+        max: 4500
+      }
     },{
       id: 1096,
-      map: "tnud",
-      name: "tnud",
+      map: "pay_fild04",
+      name: "Angeling",
       respawn: {
         min: 3600,
         max: 5400
       }
     }, {
       id: 1096,
-      map: "Chevre",
-      name: "Herb",
+      map: "xmas_dun01",
+      name: "Angeling",
       respawn: {
         min: 3600,
         max: 5400
       }
     }, {
       id: 1096,
-      map: "Suleimann III",
-      name: "Sulei",
+      map: "yuno_fild03",
+      name: "Angeling",
       respawn: {
         min: 3600,
         max: 5400
       }
     }, {
       id: 1388,
-      map: "Xsed",
-      name: "Jelly",
+      map: "yuno_fild05",
+      name: "Archangeling",
       respawn: {
         min: 3600,
         max: 3780
       }
     }, {
       id: 1785,
-      map: "ReijiMaigo",
-      name: "Reiji",
+      map: "ra_fild02",
+      name: "Atroce",
       respawn: {
         min: 14400,
         max: 15e3
       }
     }, {
       id: 1785,
-      map: "Yukimie",
-      name: "Ev",
+      map: "ra_fild03",
+      name: "Atroce",
       respawn: {
         min: 10800,
         max: 11400
       }
     }, {
       id: 1785,
-      map: "Richies",
-      name: "Chimong",
+      map: "ra_fild04",
+      name: "Atroce",
       respawn: {
         min: 18e3,
         max: 18600
       }
     }, {
       id: 1785,
-      map: "Zhade",
-      name: "Abink",
+      map: "ve_fild01",
+      name: "Atroce",
       respawn: {
         min: 10800,
         max: 11400
       }
     }, {
       id: 1785,
-      map: "Blade Hunter",
-      name: "Burak",
+      map: "ve_fild02",
+      name: "Atroce",
       respawn: {
         min: 21600,
         max: 22200
       }
     }, {
       id: 1630,
-      map: "DarkMinos",
-      name: "Minos",
+      map: "lou_dun03",
+      name: "White Lady",
       respawn: {
         min: 7020,
         max: 7620
       }
     }, {
       id: 1039,
-      map: "Tainted~",
-      name: "Kriszke",
+      map: "prt_maze03",
+      name: "Baphomet",
       respawn: {
         min: 6300,
         max: 8100
       }
     }, {
       id: 1873,
-      map: "Bellucci",
-      name: "Masha",
+      map: "abbey03",
+      name: "Beelzebub",
       respawn: {
         min: 36000,
         max: 50400
       }
     }, {
       id: 1646,
-      map: "Powerbauer",
-      name: "Johnny",
+      map: "lhz_dun03",
+      name: "Bio 3 MVP",
       respawn: {
         min: 6e3,
         max: 7800
       }
     }, {
       id: 2231,
-      map: "Fiel Nirvalen",
-      name: "Fiel",
+      map: "lhz_dun04",
+      name: "Bio 4 MVP",
       respawn: {
         min: 6e3,
         max: 7800
       }
     }, {
       id: 2068,
-      map: "Tweet Mona",
-      name: "Mona",
+      map: "bra_dun02",
+      name: "Boitata",
       respawn: {
         min: 6300,
         max: 8100
       }
     }, {
       id: 1272,
-      map: "Yumelina",
-      name: "Mochina ",
+      map: "gl_chyard",
+      name: "Dark Lord [DL]",
       respawn: {
         min: 2700,
         max: 4500
       }
     }, {
       id: 1719,
-      map: "Aegor",
-      name: "Amps",
+      map: "abyss_03",
+      name: "Detale",
       respawn: {
         min: 9900,
         max: 11700
       }
     }, {
       id: 1582,
-      map: "Billy Theo",
-      name: "Theo",
+      map: "pay_fild04",
+      name: "Deviling",
       respawn: {
         min: 7200,
         max: 10800
       }
     }, {
       id: 1582,
-      map: "Viotto",
-      name: "Vio",
+      map: "yuno_fild03",
+      name: "Deviling",
       respawn: {
         min: 3600,
         max: 5400
       }
     }, {
       id: 1046,
-      map: "Callisto [SG]",
-      name: "Callisto",
+      map: "gef_dun02",
+      name: "Doppelganger",
       respawn: {
         min: 6300,
         max: 8100
       }
     }, {
       id: 1389,
-      map: "Heftridge",
-      name: "Hefty",
+      map: "gef_dun01",
+      name: "Dracula",
       respawn: {
         min: 2700,
         max: 4500
       }
     }, {
       id: 1112,
-      map: "Schleppy*",
-      name: "Schleppy",
+      map: "treasure02",
+      name: "Drake",
       respawn: {
         min: 6300,
         max: 8100
       }
     }, {
       id: 1115,
-      map: "Kutsuru",
-      name: "Kuts",
+      map: "pay_fild11",
+      name: "Eddga",
       respawn: {
         min: 6300,
         max: 8100
       }
     }, {
       id: 1652,
-      map: "HolyPanda26",
-      name: "Panda",
+      map: "lhz_dun02",
+      name: "Egnigem Cenia [GEC]",
       respawn: {
         min: 6300,
         max: 8100
       }
-    }
+    }, {
+      id: 1418,
+      map: "gon_dun03",
+      name: "Evil Snake Lord [ESL]",
+      respawn: {
+        min: 5640,
+        max: 6240
+      }
+    }, {
+      id: 1871,
+      map: "abbey02",
+      name: "Fallen Bishop Hibram [FBH]",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1252,
+      map: "xmas_fild01",
+      name: "Garm",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1120,
+      map: "pay_fild04",
+      name: "Ghostring",
+      respawn: {
+        min: 3600,
+        max: 5400
+      }
+    }, {
+      id: 1120,
+      map: "prt_maze03",
+      name: "Ghostring",
+      respawn: {
+        min: 6780,
+        max: 10200
+      }
+    }, {
+      id: 1120,
+      map: "treasure02",
+      name: "Ghostring",
+      respawn: {
+        min: 1980,
+        max: 3180
+      }
+    }, {
+      id: 1768,
+      map: "ra_san05",
+      name: "Gloom Under Night",
+      respawn: {
+        min: 18e3,
+        max: 19800
+      }
+    }, {
+      id: 1086,
+      map: "prt_sewb4",
+      name: "Golden Thief Bug [GTB]",
+      respawn: {
+        min: 2700,
+        max: 4500
+      }
+    }, {
+      id: 1885,
+      map: "mosk_dun03",
+      name: "Gopinich",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1990,
+      map: "man_fild03",
+      name: "Hardrock Mammoth",
+      respawn: {
+        min: 14400,
+        max: 14460
+      }
+    }, {
+      id: 1832,
+      map: "thor_v03",
+      name: "Ifrit",
+      respawn: {
+        min: 32400,
+        max: 46800
+      }
+    }, {
+      id: 1492,
+      map: "ama_dun03",
+      name: "Incantation Samurai",
+      respawn: {
+        min: 5460,
+        max: 6060
+      }
+    }, {
+      id: 1734,
+      map: "kh_dun02",
+      name: "Kiel D-01",
+      respawn: {
+        min: 7200,
+        max: 10800
+      }
+    }, {
+      id: 2202,
+      map: "iz_dun05",
+      name: "Kraken",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1779,
+      map: "ice_dun03",
+      name: "Ktullanux",
+      respawn: {
+        min: 7200,
+        max: 7260
+      }
+    }, {
+      id: 1688,
+      map: "ayo_dun02",
+      name: "Lady Tanee",
+      respawn: {
+        min: 21600,
+        max: 28800
+      }
+    }, {
+      id: 2156,
+      map: "dew_dun01",
+      name: "Leak",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1373,
+      map: "niflheim",
+      name: "Lord of Death [LOD]",
+      respawn: {
+        min: 7980,
+        max: 8040
+      }
+    }, {
+      id: 1147,
+      map: "anthell02",
+      name: "Maya",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1289,
+      map: "anthell01",
+      name: "Maya Purple",
+      respawn: {
+        min: 7200,
+        max: 10800
+      }
+    }, {
+      id: 1059,
+      map: "mjolnir_04",
+      name: "Mistress",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1150,
+      map: "pay_dun04",
+      name: "Moonlight Flower",
+      respawn: {
+        min: 2700,
+        max: 4500
+      }
+    }, {
+      id: 1087,
+      map: "gef_fild02",
+      name: "Orc Hero",
+      respawn: {
+        min: 86400,
+        max: 87e3
+      }
+    }, {
+      id: 1087,
+      map: "gef_fild14",
+      name: "Orc Hero",
+      respawn: {
+        min: 2700,
+        max: 4500
+      }
+    }, {
+      id: 1190,
+      map: "gef_fild10",
+      name: "Orc Lord",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1038,
+      map: "moc_pryd04",
+      name: "Osiris",
+      respawn: {
+        min: 3600,
+        max: 10800
+      }
+    }, {
+      id: 1157,
+      map: "in_sphinx5",
+      name: "Pharaoh",
+      respawn: {
+        min: 2700,
+        max: 4500
+      }
+    }, {
+      id: 1159,
+      map: "moc_fild17",
+      name: "Phreeoni",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 2087,
+      map: "dic_dun02",
+      name: "Queen Scaraba",
+      respawn: {
+        min: 7200,
+        max: 7260
+      }
+    }, {
+      id: 2165,
+      map: "dic_dun03",
+      name: "Gold Queen Scaraba",
+      respawn: {
+        min: 7200,
+        max: 7260
+      }
+    }, {
+      id: 1623,
+      map: "ein_dun02",
+      name: "RSX-0806",
+      respawn: {
+        min: 6600,
+        max: 8400
+      }
+    }, {
+      id: 1916,
+      map: "moc_fild21",
+      name: "Satan Morroc [SM]",
+      respawn: {
+        min: 43200,
+        max: 54e3
+      }
+    }, {
+      id: 1251,
+      map: "xmas_dun02",
+      name: "Stormy Knight [SK]",
+      respawn: {
+        min: 2700,
+        max: 4500
+      }
+    }, {
+      id: 1583,
+      map: "beach_dun",
+      name: "Tao Gunka (West) [TGK]",
+      respawn: {
+        min: 17100,
+        max: 18900
+      }
+    }, {
+      id: 1583,
+      map: "beach_dun2",
+      name: "Tao Gunka (North) [TGK]",
+      respawn: {
+        min: 17100,
+        max: 18900
+      }
+    }, {
+      id: 1583,
+      map: "beach_dun3",
+      name: "Tao Gunka (East) [TGK]",
+      respawn: {
+        min: 17100,
+        max: 18900
+      }
+    }, {
+      id: 1991,
+      map: "spl_fild03",
+      name: "Tendrillion",
+      respawn: {
+        min: 3600,
+        max: 3660
+      }
+    }, {
+      id: 1708,
+      map: "thana_boss",
+      name: "Thanatos",
+      respawn: {
+        min: 7200,
+        max: 7260
+      }
+    }, {
+      id: 1312,
+      map: "tur_dun04",
+      name: "Turtle General [TG]",
+      respawn: {
+        min: 2700,
+        max: 4500
+      }
+    }, {
+      id: 1751,
+      map: "odin_tem03",
+      name: "Valkyrie Randgris [VR]",
+      respawn: {
+        min: 28800,
+        max: 50400
+      }
+    }, {
+      id: 1685,
+      map: "jupe_core",
+      name: "Vesper",
+      respawn: {
+        min: 6300,
+        max: 8100
+      }
+    }, {
+      id: 1917,
+      map: "moc_fild22",
+      name: "Wounded Morroc [WM]",
+      respawn: {
+        min: 43200,
+        max: 54e3
+      }
     }]
   }, d.get = function(b) {
     var c = a.database().ref(b);
